8ece1f0755e2d2cf690bf13b95669e614a867cc9
Hopeless Future <frankierafie12@gmail.com>
2024-06-02 23:06:37 +0300

    Initial commit

---
+++ app/src/main/java/com/kotlity/allpractice/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlity.allpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.kotlity.allpractice.ui.theme.AllPracticeTheme
+import com.kotlity.different_screen_sizes.screens.DifferentScreenSizesHandler
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AllPracticeTheme {
+                DifferentScreenSizesHandler()
+            }
+        }
+    }
+}
